cb1f5f9dd80af52b7f802f2a178b4ae7311695f1
Kristian Jay Tadeo <107788212+KrisChan33@users.noreply.github.com>
2023-06-03 11:24:58 +0800

    Create Clear All Temporary Files every 5 seconds.bat
    This is a Cleaner that Automatically delete all the temporary files stored in your pc.  it delete every 5 seconds.

---
+++ Bash Files/Bash Scripts/Clear All Temporary Files every 5 seconds.bat
@@ -0,0 +1,77 @@
+#Developer: Kristian Jay Tadeo 
+#Date Developed: 8/3/22
+#MIT License
+
+
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+mode con: cols=60 lines=30
+
+:loop 
+timeout %INTERVAL%  
+
+color 0e
+del /s /f /q %windir%\temp\*.*    
+rd /s /q %windir%\temp    
+md %windir%\temp    
+del /s /f /q %windir%\Prefetch\*.*    
+rd /s /q %windir%\Prefetch    
+md %windir%\Prefetch    
+del /s /f /q %windir%\system32\dllcache\*.*    
+rd /s /q %windir%\system32\dllcache    
+md %windir%\system32\dllcache    
+del /s /f /q "%SystemDrive%\Temp"\*.*    
+rd /s /q "%SystemDrive%\Temp"    
+md "%SysteDrive%\Temp"    
+del /s /f /q %temp%\*.*    
+rd /s /q %temp%    
+md %temp%    
+del /s /f /q "%USERPROFILE%\Local Settings\History"\*.*    
+rd /s /q "%USERPROFILE%\Local Settings\History"    
+md "%USERPROFILE%\Local Settings\History"    
+del /s /f /q "%USERPROFILE%\Local Settings\Temporary Internet Files"\*.*    
+rd /s /q "%USERPROFILE%\Local Settings\Temporary Internet Files"    
+md "%USERPROFILE%\Local Settings\Temporary Internet Files"    
+del /s /f /q "%USERPROFILE%\Local Settings\Temp"\*.*    
+rd /s /q "%USERPROFILE%\Local Settings\Temp"    
+md "%USERPROFILE%\Local Settings\Temp"    
+del /s /f /q "%USERPROFILE%\Recent"\*.*    
+rd /s /q "%USERPROFILE%\Recent"    
+md "%USERPROFILE%\Recent"    
+del /s /f /q "%USERPROFILE%\Cookies"\*.*    
+rd /s /q "%USERPROFILE%\Cookies"    
+md "%USERPROFILE%\Cookies"
+
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls   
+
+:del /s /f /q %userprofile%\Recent\*.*
+
+:del /s /f /q C:\Windows\Prefetch\*.*
+
+:del /s /f /q C:\Windows\Temp\*.*
+
+:del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+
+@echo off 
+color 0e 
+set INTERVAL= 5
+
+::Delay
+::ping -n 5 127.0.0.1>nul
+goto:loop
